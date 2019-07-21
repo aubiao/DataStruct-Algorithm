85b36ff32f5f992097ff581cb415e4b364b7a692
Patrick <923079848@qq.com>
2019-07-21 13:05:50 +0800

    添加二叉树排序

---
+++ Base/Tree/BinaryTreeSort.go
@@ -0,0 +1,38 @@
+//二叉树实现排序
+package Tree
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, v int) *tree {
+	if t == nil {
+		t = new(tree)
+		t.value = v
+		return t
+	}
+	if v < t.value {
+		t.left = add(t.left, v)
+	} else {
+		t.right = add(t.right, v)
+	}
+	return t
+}
